b45eb6ce20ef1f79481db93c0da82ca3496182d1
Ace Greathouse <acegreathouse75@gmail.com>
2026-02-23 20:10:44 -0500

    Clean data anomalies

--- fakedata.py
+++ fakedata.py
@@ -4,38 +4,82 @@ import random
 import argparse
 import sys
 from faker import Faker
+from faker.config import AVAILABLE_LOCALES
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
+import difflib
+import pycountry_convert as pc
+from geonames_addr import GeoLocator
 
+#Functions
 def locale_fnct(x):
-  #split the input argument into two elements either using and underscore (_) or a dash (-) as the separator
-  return x.split("_",2) if x.find("_") != -1 else (x.split("-",2) if x.find("-") != -1 else -1)
+  #Standardize input to use underscores, then split
+  normalized = x.replace('-','_')
+  parts = normalized.split('_')
+
+  #Ensure we always return a list of at least two items to avoid index errors
+  if len(parts) < 2:
+    return [parts[0], "UNKNOWN"]
+  return parts
+
+def get_continent(country_code):
+  try:
+    # Convert country code (e.g., 'US') to continent code (e.g., 'NA')
+    continent_code = pc.country_alpha2_to_continent_code(country_code)
+
+    #Convert continent code to full name
+    continent_names = {
+      'AF': 'Africa',
+      'AS': 'Asia',
+      'EU': 'Europe',
+      'NA': 'North America',
+      'OC': 'Oceania',
+      'SA': 'South America',
+      'AN': 'Antarctica'
+    }
+    return continent_names[continent_code]
+  except KeyError:
+    suggestions = difflib.get_close_matches(country_code, continent_names.keys(), n=1)
+    if not suggestions:
+      return "Unknown Country Code"
+    else:
+      return f"Unknown Country Code.  Did you mean {suggestions[0]}"
+
 
 #******Define Argument Parser******
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="method to generate fake PII data")
   parser.add_argument("--locale", type=str, default = 'en-US', help="The first argument specifying a language and country to use for generating data. [default: en-US]")
-  parser.add_argument("--record_number", type=int, default = 100, help="The second argument specifying the nubmer of rows desired in output [default: 10]")
+  parser.add_argument("--record_number", type=int, default = 100, help="The second argument specifying the number of rows desired in output [default: 100]")
 
 #******Parse arguments
 args = parser.parse_args()
 
 #Save inputs
-locale = args.locale  #'en-US' en-GB
+locale = args.locale  #'en-US' 'en-CA' 'en-GB' 'it_IT' 'nl_NL' 'fr_FR 'de_DE' 'en_AU'
 rec_num = args.record_number
+supported_locales = AVAILABLE_LOCALES
+
 
 #split the input argument into two elements either using and underscore (_) or a dash (-) as the separator
 #locale_fnct = lambda x: x.split("_",2) if x.find("_") != -1 else (x.split("-",2) if x.find("-") != -1 else -1)
 
 locale_array = locale_fnct(locale)
+clean_locale = locale_array[0] + '_' + locale_array[1]
+country_cd = locale_array[1]
 
-#Validate locale argument
-if locale_array[0] == 'en' and locale_array[1] in ('US','CA','UK','GB'):
-  fake = Faker(locale)
+#Validate locale argument no matter what the separator is
+if (clean_locale) in supported_locales:
+  fake = Faker(clean_locale)
 else:
-  raise ValueError(f"Currently, the process only accepts en for the language and UK, GB, US or CA for the country code.  You provided {locale}")
+  suggestions = difflib.get_close_matches(clean_locale, supported_locales, n=1)
+  if not suggestions:
+    raise ValueError(f"Currently, the process only accepts the following for the locales: {supported_locales}.  You provided {locale}")
+    sys.exit(1)
+  print(f"Did you mean '{suggestions[0]}")
   sys.exit(1)
+
 
 #Validate number of rows argument
 if isinstance(rec_num, int):
@@ -46,38 +90,34 @@ else:
 
 fake_data = []
 
+locator = GeoLocator([country_cd])
+continent = get_continent(country_cd) 
+
+Faker.seed(100)
+
 #******Generate Data based on argument input********
 for i in range(num_rows):
-  Faker.seed(i)
+  
+  loc = locator.get_random_location(country_cd)
+  loc_df = pd.Series(loc)
+
+  #While there is NAN data in the result, get another result for 5 tries
+  cnt = 0
+  while loc_df.isna().sum().sum() > 0 and cnt < 5:
+    loc = locator.get_random_location(country_cd)
+    loc_df = pd.Series(loc)
+    cnt+=1
+
+  if cnt > 4:
+    print(f"Retries for clean data maxed out at {cnt} tries.")
 
   #Define fake data
   street_address = fake.street_address()
-  city = fake.city()
-
-  if locale_array[1] in ('GB', 'UK'):
-    state = 'N/A'
-    county = fake.county()
-    province = 'N/A'
-    country = 'United Kingdom'
-    postal_code = fake.postcode()
-    cell_phone = fake.cellphone_number()
-
-  if locale_array[1] == 'US': 
-    state = fake.state()
-    county = 'N/A'
-    province = 'N/A'
-    country = 'United States'
-    postal_code = fake.postalcode()
-    cell_phone = fake.basic_phone_number()
-
-  if locale_array[1] == 'CA':
-    state = 'N/A'
-    county = 'N/A'
-    province = fake.province()
-    country = 'Canada'
-    postal_code = fake.postalcode()
-    cell_phone = 'N/A'
-  
+  city = loc['city']
+  state = loc['state_province']
+  state_code = loc['state_abbr']
+  postal_code = loc['postal_code']
+  country = loc['country']
   email = fake.free_email()
   phone_number = fake.phone_number()
   gender = random.choice(['M', 'F'])
@@ -88,16 +128,19 @@ for i in range(num_rows):
 
   new_row_data = {
       'name': person['name'],
-      'street_address': street_address,
+      'street_address': street_address.replace('\n',' '),
       'city': city,
-      'county': county,
-      'province': province,
+      #'county': county,
+      #'province': province,
       'state': state,
+      'state_code': state_code,
       'postal_code': postal_code,
+      #'address': fake.address(),
       'country': country,
+      'continent': continent,
       'email': email,
       'phone': phone_number,
-      'cell_phone': cell_phone,
+      #'cell_phone': cell_phone,
       'gender': gender,
       'dob': dob
     }
@@ -106,7 +149,7 @@ for i in range(num_rows):
 dir_path = './output/data/'
 now = datetime.now()
 now_str = now.strftime('%Y_%m_%d %H:%M:%S')
-file_name = f"fake_data_{locale_array[1]}_{now_str}.csv"
+file_name = f"fake_data_{country_cd}_{now_str}.csv"
 file_path = dir_path + file_name
 rel_file_path = Path(file_path)
 full_path = rel_file_path.resolve()
---
+++ rmv_chrs.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from datetime import datetime
+import pandas as pd
+
+#Define file to ingest
+def get_file (file_name, dir_path="./output/data/"):
+    file_path = Path(dir_path + file_name)
+    df = pd.read_csv(
+            file_path, sep=',', dtype=str, low_memory=False
+    )
+    return df
+
+loc = "./output/data/"
+file = "fake_data_GB_2026_02_22 22:08:18.csv"
+gb_df = get_file(file, loc)
+
+print(f"Before replacement:\n {gb_df.head(10)}")
+
+gb_df['street_address'] = gb_df['street_address'].replace('\n', ' ',regex=True)
+
+print(f"After replacement:\n {gb_df.head(10)}")
+
+upd_file_path = Path(loc + file.replace(".",f"_updated_{datetime.now().strftime('%Y_%m_%d %H:%M:%S')}."))
+
+gb_df.to_csv(upd_file_path,index=False)
