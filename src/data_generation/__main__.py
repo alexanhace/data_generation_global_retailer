625406adb7b1d9d70a87f5ec898365158b0c9211
Ace Greathouse <acegreathouse75@gmail.com>
2026-02-24 20:36:56 -0500

    mutiple: dir struct;seeding;logging;file name;main function

--- src/data_generation/__main__.py
+++ src/data_generation/__main__.py
@@ -1,9 +1,7 @@
 # TODO: Update the main function to your needs or remove it.
 
 
-def main() -> None:
-    print("Start coding in Python today!")
-
+from data_generation.customer_fakedata import main
 
 if __name__ == "__main__":
     main()
